ca022fca3adce7817cecb0ea13dbc6ceb0e54748
JMyers79 <joshua.myers0311@gmail.com>
2024-10-30 16:57:20 -0400

    Update select.js

--- select.js
+++ select.js
@@ -3,6 +3,7 @@ $('#name').text('Joshua');
 $('#major').text('CIT Web/Software Dev');
 $('#year').text('Sophomore');
 
-$('h4').html('<em>Home State: Indiana</em>');}
+$('h4').html('<em>Home State: Indiana</em>');};
 
-setUp()
+$(document).ready(setUp);
+
